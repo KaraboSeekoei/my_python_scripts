6539450ce94e306570480e18230cfcade0d88e50
Karabo Seekoei <karaboprincess94@gmail.com>
2026-03-19 19:53:59 +0200

    Add files_by_date method to list all files with modification dates

--- file_listing.py
+++ file_listing.py
@@ -1,15 +1,26 @@
 from pathlib import Path
+from datetime import datetime, date
 
-folder = Path(".")
-
-def files_in_folder():
+def files_in_folder(folder_path):
+    folder = Path(folder_path)
     for file in folder.iterdir():
         if file.is_file():
             print(file.name)
 
 def list_files_by_ext(folder_path,ext_tuple):
-    folder = Path(folder_path)
+    folder = Path(folder_path) #Remember Path converts strings to objects so that we can access them
     for file in folder.iterdir():
         if file.is_file() and file.name.endswith(ext_tuple):
             print(file.name)
-list_files_by_ext("BasicMath",".py")
+
+def files_by_date(folder_path, ext_tuple):
+    folder =  Path(folder_path)
+    for file in folder.iterdir():
+        if file.is_file() and file.name.endswith(ext_tuple):
+            mod_time = datetime.fromtimestamp(file.stat().st_mtime).date()
+            print(f"{file.name } - {mod_time}")
+files_by_date("BasicMath", ".py")
+
+
+
+# list_files_by_ext("BasicMath",".py")
